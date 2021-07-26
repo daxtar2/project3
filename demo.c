776356992acff44dbe4399572f20c1329fa1c4e7
daxtar2 <87515461+daxtar2@users.noreply.github.com>
2021-07-26 20:58:55 +0800

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define PI 3.14
+//宏定义
+int main(void)
+{
+	int a = 1;
+	//不规范变量名,不清楚意思，应见名知意
+	int sun_flower = 50;
+	//下划线命名法
+
+	int sunFlowervalue = 100;
+	//驼峰命名法perl
+	
+
+}
